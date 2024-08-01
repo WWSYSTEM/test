06cb36e2574f46b8a3c13b1ce2cc2a875a09ec80
wwsystem <wwsystem@wwsystem.com.pl>
2024-08-01 10:54:51 +0200

    ad

---
+++ main.py
@@ -0,0 +1,21 @@
+﻿# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    
+    count = 0
+    while True:
+     print_hi(f'PyCharm ${count}')
+     count += 1
+     time.sleep(1)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
